0056e7c5169e19c47ce35e487da2250cc28c7c84
Gabriel Erick <seu-email@exemplo.com>
2024-11-25 20:28:50 -0300

    listando serviços

--- app_bookmenow/lib/home_page.dart
+++ app_bookmenow/lib/home_page.dart
@@ -9,6 +9,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final List<Map<String, dynamic>> servicos = [
+    {
+      "titulo": "Corte de cabelo",
+      "descricao":
+          "a descrica desse texto foi um teste para ver se da certo o terreno para o json",
+      "preco": 50.00,
+      "imagem": "https://via.placehodler.com/150"
+    },
+    {
+      "titulo": "Mecanico",
+      "descricao": "um teste para ver se da certo o terreno para o json",
+      "preco": 120.00,
+      "imagem": "https://via.placehodler.com/150"
+    },
+    {
+      "titulo": "Corte de cabelo",
+      "descricao":
+          "a descrica desse texto foi um teste para ver se da certo o terreno para o json",
+      "preco": 50.00,
+      "imagem": "https://via.placehodler.com/150"
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,6 +83,31 @@ class _HomePageState extends State<HomePage> {
             ),
           ],
         ),
+      ),
+      // itemCount é para dizer a quantidade que queremos listar (no caso, tem que ser dinâmico, ent, o tamanho do meu objeto declarado lá em cima)
+      // context é tudo o que tem na página, index é o indíce (como em um array), como tem 4 items no meu itemCount, ele irá repetir o itemBuilder 4 vezes
+      body: ListView.builder(
+        itemCount: servicos.length,
+        itemBuilder: (context, index) {
+          // Cada serviço será armazenado na variável servico enquanto servicos é percorrido
+          final servico = servicos[index];
+          return Card(
+            margin: const EdgeInsets.all(8.0),
+            color: const Color(0xFFfcfcfc),
+            // criando uma linha, pois vou querer elementos um ao lado do outro, e ela faz isso (coluna deixa um abaixo do outro)
+            child: Row(
+              children: [
+                // vai ter uma imagem da internet, pois o campo 'imagemUrl' do meu objeto servicos, contém um link da internet que manda para uma imagem
+                Image.network(
+                  servico['imagemUrl'],
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover, // vai ajustar a imagem ao centro do espaço em que ela está
+                ),
+              ],
+            ),
+          );
+        },
       ),
     );
   }
